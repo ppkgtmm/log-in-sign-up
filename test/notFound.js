261f22e91d3029d19f184d058ca75730c3eb6858
ppkgtmm <pkgtm1998@gmail.com>
2020-11-05 16:34:58 +0700

    edit ci.yml

--- test/notFound.js
+++ test/notFound.js
@@ -7,40 +7,44 @@ chai.should()
 
 describe("Test not found 404", () => {
     it("test wrong method", async () => {
-        return chai.request(server).post("/").send({
-            abcd: "123"
-        }).then((res) => {
-            res.should.have.status(404)
-            res.body.should.be.a("object")
-            res.body.should.have.property("error")
-        })
+        return chai.request(server).post("/")
+            .send({
+                abcd: "123"
+            }).then((res) => {
+                res.should.have.status(404)
+                res.body.should.be.a("object")
+                res.body.should.have.property("error")
+            })
     })
 
     it("test wrong method v1", async () => {
-        return chai.request(server).delete("/v1/signup").send({
-            abcd: "123"
-        }).then((res) => {
-            res.should.have.status(404)
-            res.body.should.be.a("object")
-            res.body.should.have.property("error")
-        })
+        return chai.request(server).delete("/v1/signup")
+            .send({
+                abcd: "123"
+            }).then((res) => {
+                res.should.have.status(404)
+                res.body.should.be.a("object")
+                res.body.should.have.property("error")
+            })
     })
 
     it("test wrong endpoint", async () => {
-        return chai.request(server).get("/v2").then((res) => {
-            res.should.have.status(404)
-            res.body.should.be.a("object")
-            res.body.should.have.property("error")
-        })
+        return chai.request(server).get("/v2")
+            .then((res) => {
+                res.should.have.status(404)
+                res.body.should.be.a("object")
+                res.body.should.have.property("error")
+            })
     })
 
     it("test wrong endpoint v1", async() => {
-        return chai.request(server).post("/v1/signin").send({
-            abcd: "123"
-        }).then((res) => {
-            res.should.have.status(404)
-            res.body.should.be.a("object")
-            res.body.should.have.property("error")
-        })
+        return chai.request(server).post("/v1/signin")
+            .send({
+                abcd: "123"
+            }).then((res) => {
+                res.should.have.status(404)
+                res.body.should.be.a("object")
+                res.body.should.have.property("error")
+            })
     })
 })
--- test/userAPI.js
+++ test/userAPI.js
@@ -31,50 +31,54 @@ function responseDataTester(res, expected) {
 describe("Test user API",() =>{
     describe("Test sign up", () => {
         it("Case no data provided", async () => {
-            return chai.request(server).post("/v1/signup/").then((res) => {
-                const keys = ["firstname", "lastname", "email", "password"]
-                responseTester(res, { status: 400 })
-                responseDataTester(res, { field: "messages",  keys })
-            })
+            return chai.request(server).post("/v1/signup/")
+                .then((res) => {
+                    const keys = ["firstname", "lastname", "email", "password"]
+                    responseTester(res, { status: 400 })
+                    responseDataTester(res, { field: "messages",  keys })
+                })
         })
     
         describe("Valid email", () => {
             it("Not enough data provided", async () => {
-                return chai.request(server).post("/v1/signup/").send({
-                    ...testData,
-                    firstname: undefined,
-                    lastname: "",
-                    password: ""
-                }).then((res) => {
-                    const keys = ["firstname", "lastname", "password"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
-                })
+                return chai.request(server).post("/v1/signup/")
+                    .send({
+                        ...testData,
+                        firstname: undefined,
+                        lastname: "",
+                        password: ""
+                    }).then((res) => {
+                        const keys = ["firstname", "lastname", "password"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
+                    })
             })
             it("Invalid data provided", async () => {
-                return chai.request(server).post("/v1/signup/").send({
-                    ...testData,
-                    password: "2345",
-                    abc: "><><"
-                }).then((res) => {
-                    const keys = ["password"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
-                })
+                return chai.request(server).post("/v1/signup/")
+                    .send({
+                        ...testData,
+                        password: "2345",
+                        abc: "><><"
+                    }).then((res) => {
+                        const keys = ["password"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
+                    })
             })
     
             it("Not enough and invalid data provided", async () => {
-                return chai.request(server).post("/v1/signup/").send({
-                    ...testData,
-                    firstname: "",
-                    lastname:  randomString({length: 135}),
-                    password: "2345",
-                    abc: "><><"
-                }).then((res) => {
-                    const keys = ["firstname", "lastname", "password"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
-                })
+                return chai.request(server).post("/v1/signup/")
+                    .send({
+                        ...testData,
+                        firstname: "",
+                        lastname:  randomString({length: 135}),
+                        password: "2345",
+                        abc: "><><"
+                    }).then((res) => {
+                        const keys = ["firstname", "lastname", "password"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
+                    })
             })
         
             it("Valid and enough data provided", async () => {
@@ -179,54 +183,59 @@ describe("Test user API",() =>{
     describe("Test login", () => {
         describe("not enough data provided", () => {
             it("Nothing provided", async () => {
-                return chai.request(server).post("/v1/login/").then((res) => {
-                    const keys = ["error"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
-                })
+                return chai.request(server).post("/v1/login/")
+                    .then((res) => {
+                        const keys = ["error"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
+                    })
             })
             it("No password provided", async () => {
-                return chai.request(server).post("/v1/login/").send({
-                    email: testData.email
-                }).then((res) => {
-                    const keys = ["error"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
-                })
+                return chai.request(server).post("/v1/login/")
+                    .send({
+                        email: testData.email
+                    }).then((res) => {
+                        const keys = ["error"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
+                    })
             })
         })
 
         describe("Enough data provided", () => {
             it("User doesn't exist", async () => {
-                return chai.request(server).post("/v1/login/").send({
-                    email: "abcdef4@"
-                }).then((res) => {
-                    const keys = ["error"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
-                })
+                return chai.request(server).post("/v1/login/")
+                    .send({
+                        email: "abcdef4@"
+                    }).then((res) => {
+                        const keys = ["error"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
+                    })
             })
     
             it("User exist but incorrect password", async () => {
-                return chai.request(server).post("/v1/login/").send({
-                    ...testData,
-                    password: "1234567"
-                }).then((res) => {
-                    const keys = ["error"]
-                    responseTester(res, { status: 400 })
-                    responseDataTester(res, { field: "messages",  keys })
+                return chai.request(server).post("/v1/login/")
+                    .send({
+                        ...testData,
+                        password: "1234567"
+                    }).then((res) => {
+                        const keys = ["error"]
+                        responseTester(res, { status: 400 })
+                        responseDataTester(res, { field: "messages",  keys })
 
-                })
+                    })
             })
             
             it("User exist and correct password", async () => {
-                return chai.request(server).post("/v1/login/").send({
-                    ...testData
-                }).then((res) => {
-                    const keys = ["firstname", "lastname", "email", "token"]
-                    responseTester(res, { status: 200 })
-                    responseDataTester(res, { field: "data",  keys })
-                })
+                return chai.request(server).post("/v1/login/")
+                    .send({
+                        ...testData
+                    }).then((res) => {
+                        const keys = ["firstname", "lastname", "email", "token"]
+                        responseTester(res, { status: 200 })
+                        responseDataTester(res, { field: "data",  keys })
+                    })
             })
         })
     })
